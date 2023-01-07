4a8c71ba7e3c89aadeaada255c42a04538d9164f
brokenKickstand <rtripoli5@gmail.com>
2023-01-07 13:41:53 -0600

    added word banks with random generated word functions

--- mixed-message.js
+++ mixed-message.js
@@ -1,5 +1,11 @@
+const { randomVerb, randomName, randomAdj} = require("./message-arrays")
 // generates a random phrase everytime the program is run
 // constraint requires 3 different parts of the message to be randomized
+const messageOutlines = [
+    `${randomName()} has been wanting to go and ${randomVerb()} the giant with ${randomName()}. They're both very ${randomAdj()}.`,
+    `${randomName()} is the most ${randomAdj()} person you could ever meet. One time I saw him ${randomVerb()} a bowling ball with his hand.`
+];
+
 const sendMixedMessage = (a, b, c) => {
 
 }
