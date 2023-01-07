1685b070f7d9be9ea528fafcfd38d6cec843bf08
brokenKickstand <rtripoli5@gmail.com>
2023-01-07 13:46:19 -0600

    added function to generate mixed-message. It will provide some kind of random madlib

--- mixed-message.js
+++ mixed-message.js
@@ -3,9 +3,12 @@ const { randomVerb, randomName, randomAdj} = require("./message-arrays")
 // constraint requires 3 different parts of the message to be randomized
 const messageOutlines = [
     `${randomName()} has been wanting to go and ${randomVerb()} the giant with ${randomName()}. They're both very ${randomAdj()}.`,
-    `${randomName()} is the most ${randomAdj()} person you could ever meet. One time I saw him ${randomVerb()} a bowling ball with his hand.`
+    `${randomName()} is the most ${randomAdj()} person you could ever meet. One time I saw him ${randomVerb()} a bowling ball with his hand.`,
+    `${randomName()} and ${randomName()} will ${randomVerb()} on an adventure together. They both will ${randomVerb()}`
 ];
 
-const sendMixedMessage = (a, b, c) => {
-
+const sendMixedMessage = () => {
+    return messageOutlines[Math.floor(Math.random() * messageOutlines.length)];
 }
+
+console.log(sendMixedMessage());
